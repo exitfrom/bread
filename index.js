1ea16a01cf88c0c62855705692e107ecc1fb680e
Bestian Tang <bestian@gmail.com>
2015-01-11 09:52:14 +0800

    地基
    資料請趕快補上

--- index.js
+++ index.js
@@ -17,15 +17,35 @@ function myCtrl ($scope) {
     '悠閒',
     '情傷',
     ],
-    
-    breads: [
-      {name:'', img:'', mood: ''},
-      {},
-      {}
 
-      /*  ENTER YOUR DATA HERE USING A LIST OF OBJECTS  */
+    breads: [
+      {name:'一包', img:'./images/2599.jpg',
+      	 mood: '難過+失望', text:'難過的時候，咬一咬「一包」，心情好點了嗎?'},
+
+      {name:'二包', img:'./images/2600.jpg',
+      	 mood: '開心'},
+      
+      {name:'三包', img:'./images/2603.jpg'},
+      
+
+      {name:'四包', img:'./images/2527.jpg'},
+
+      /* 請修改名字，並加入心情 */
+
     ]
 
   });
 
+
+  angular.extend($scope, { 
+  	getBread: function(){
+  		var k = $scope.key;
+  		var ls = $scope.breads.filter(function(o){
+  			var m = o.mood;
+  			return !k || (m && m.indexOf(k) > -1);
+  		})
+
+  		return ls[0] || {'name': '對不起，找不到'};
+  	}
+  });
 }
