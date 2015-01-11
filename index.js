c822399ac390ddb9bda8331eb9a1264bb6decf24
Bestian Tang <bestian@gmail.com>
2015-01-11 09:30:31 +0800

    img and mood search half

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ function myCtrl ($scope) {
 
   // models 資料
   angular.extend($scope, {  
+  	moods: [
+    '困惑',
+    '生氣',
+    '開心',
+    '難過',
+    '失望',
+    '無聊',
+    '忌妒',
+    '悠閒',
+    '情傷',
+    ],
+    
     breads: [
-      
       {name:'', img:'', mood: ''},
       {},
       {}
